ad866355dc13c042a476dc8f7efdf8b68aabc289
RalfHof <email adresse>
2024-11-22 14:44:43 +0100

    erstes Commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "GithubActionsInstanz" {
-  count = 5  
+  count = 2  
   ami           = "ami-0eddb4a4e7d846d6f"
   instance_type = "t2.micro"
   key_name = "terraformKey"
